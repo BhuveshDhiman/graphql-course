493ec5e5e2673a5a37dd2cfc0253e49d6499e567
Bhuvesh Dhiman <67685811+BhuveshDhiman@users.noreply.github.com>
2022-03-13 19:28:15 +0530

    Basic intro to graph ql

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Start your es6 scripts here
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import resolvers from "./resolvers";
+import schema from "./schema";
+
+const app = express();
+
+app.get("/", (rqe, res) => {
+  res.send("working");
+});
+
+const root = resolvers;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(8082, () => console.log("Running at 8082"));
